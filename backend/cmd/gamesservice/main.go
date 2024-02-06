2026a7271ff639920c6b9d28c27914dca449f115
EricHoelscher <40500077+EricHoelscher@users.noreply.github.com>
2024-02-05 19:32:13 -0800

    remove comment

--- backend/cmd/gamesservice/main.go
+++ backend/cmd/gamesservice/main.go
@@ -5,8 +5,6 @@ import (
 	"github.com/scott-dang/Steam-SyncUp/internal/games"
 )
 
-// The AuthService verifies OpenID information and returns whether the login is valid
-// TODO: Have it generate a token for login persistence
 func main() {
 	lambda.Start(games.Handler)
 }
